35e5505c02af06a84e27998115ce396a75587310
itamar600 <itamarzo0@gmail.com>
2019-11-24 00:13:43 +0200

    changes in myBank.c and myBank.h

--- myBank.c
+++ myBank.c
@@ -1,5 +1,6 @@
 // BASAD
 #include <stdio.h>
+#include <math.h>
 #include "myBank.h"
 #define sumAccounts 50
 #define diffArrFromAccounts 901
@@ -10,9 +11,35 @@ double accounts[sumAccounts][2] = {0};
 int sizeAccounts = 0;
 int sizeOpenAccounts = 0;
 
+int checkNumAccount(int account_num){
+       if (account_num > 950 || account_num < 901)
+    {
+        printf("Account number %d not exist\n", account_num);
+        return 1;
+    }
+    return 0;
+}
+
+int checkAmount(int amount){
+    if(amount < 0){
+         printf("The amount negative\n");
+         return 1;
+    }
+    return 0;
+
+}
+double toTwoDigits( double amount){
+    return floor(100*amount)/100;
+}
+
+
+
 open_account(double amount){
+     if(checkAmount(amount)){
+        return;
+    }
     if (sizeAccounts < sumAccounts){
-        accounts[sizeAccounts][0] = amount;
+        accounts[sizeAccounts][0] = toTwoDigits(amount);
         accounts[sizeAccounts][1] = 1;
         printf("This is your account number: %d\n" ,(sizeAccounts+diffArrFromAccounts));
         sizeAccounts++; sizeOpenAccounts++;
@@ -21,7 +48,7 @@ open_account(double amount){
         if (sizeOpenAccounts < sumAccounts){
             for(int i=0; i<sizeAccounts; i++){
                 if (accounts[i][1] == 0){
-                     accounts[i][0] = amount;
+                     accounts[i][0] = toTwoDigits(amount);
                      accounts[i][1] = 1;
                      printf("This is your account number: %d\n" ,(sieAccount+diffArrFromAccounts));
                      sizeOpenAccounts++;
@@ -45,7 +72,7 @@ depositing_money(int account_num, double amount){
     if(checkNumAccount(account_num) || checkAmount(amount)){
         return;
     }
-    accounts[account_num-diffArrFromAccounts][0] += amount;
+    accounts[account_num-diffArrFromAccounts][0] =toTwoDigits(accounts[account_num-diffArrFromAccounts][0]+ amount);
     printf("Your rest after the deposition: %lf\n", accounts[account_num-diffArrFromAccounts][0]);
 }
 
@@ -54,7 +81,7 @@ withrawal_money(int account_num, double amount){
         return;
     }
     if (accounts[account_num-diffArrFromAccounts][0] >= amount){
-        accounts[account_num-diffArrFromAccounts][0] -= amount;
+        accounts[account_num-diffArrFromAccounts][0] =toTwoDigits(accounts[account_num-diffArrFromAccounts][0]- amount) ;
         printf("Rest: %lf\n", accounts[account_num-diffArrFromAccounts][0]);
     }
     else{
@@ -86,7 +113,7 @@ close_account(int account_num){
 add_interest(double interest){
     for(int i=0; i<sizeAccounts; i++){
         if(accounts[i][1] == 1){
-            accounts[i][0] += (interest*accounts[i][0])/100;
+            accounts[i][0] =toTwoDigits(accounts[i][0]+ (interest*accounts[i][0])/100);
         }
     }
 }
@@ -106,23 +133,6 @@ close_all(){
     }
 }
 
-int checkNumAccount(int account_num){
-       if (account_num > 950 || account_num < 901)
-    {
-        printf("Account number %d not exist\n", account_num);
-        return 1;
-    }
-    return 0;
-}
-
-int checkAmount(int amount){
-    if(amount < 0){
-         printf("The amount negative\n");
-         return 1;
-    }
-    return 0;
-
-}
 
 
 
