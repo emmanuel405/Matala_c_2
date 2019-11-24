2d236295429068019aa5ed0a2015379b0c62a20d
itamar600 <itamarzo0@gmail.com>
2019-11-24 20:12:24 +0200

    to up myBank

---
+++ myBank.h
@@ -0,0 +1,22 @@
+
+int open_account(double amount);
+
+int rest(int account_num);
+
+int depositing_money(int account_num, double amount);
+
+int withrawal_money(int account_num, double amount);
+
+int close_account(int ccount_num);
+
+int add_interest(double interst);
+
+int print_open_account();
+
+int close_all();
+
+int checkAmount(int amount);
+
+int checkNumAccount(int account_num);
+
+double toTwoDigits( double amount);
